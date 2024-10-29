5321526bc2678724961760d6d8db9a8f26b4f6cb
Yogeshgouda Patil <136431159+phantomsoldierking@users.noreply.github.com>
2024-10-29 19:54:51 +0530

    Delete com/example/scheduler directory

--- com/example/scheduler/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scheduler.repository;
-
-import com.example.scheduler.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
