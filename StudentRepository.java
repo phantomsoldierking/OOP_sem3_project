2d4c00e626723af99201a4072e04cb01344fc0e1
Yogeshgouda Patil <136431159+phantomsoldierking@users.noreply.github.com>
2024-10-29 19:57:05 +0530

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scheduler.repository;
-
-import com.example.scheduler.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
