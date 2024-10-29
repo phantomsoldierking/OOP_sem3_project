3b42f2e0ace57b5e8ec0fc52e0eaa9f11bbe8575
Yogeshgouda Patil <136431159+phantomsoldierking@users.noreply.github.com>
2024-10-29 19:55:57 +0530

    Delete CourseRepository.java

--- CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.scheduler.repository;
-
-import com.example.scheduler.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
